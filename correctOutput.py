e01ed9c176e1f4deb1bab9b66c84c6df4b5a3eb9
aabhas0694 <aabhasc2@illinois.edu>
2020-07-04 21:02:30 -0500

    Fix correctOutput.py

--- correctOutput.py
+++ correctOutput.py
@@ -6,28 +6,32 @@ import os
 
 
 def main(argv):
-    input_file, entity_dictionary = '', dict()
+    input_file, entity_dictionary, entity_type_file = '', dict(), ''
     try:
-        opts, args = getopt.getopt(argv, "hi:d:", ["ifile=", "dfile="])
+        opts, args = getopt.getopt(argv, "hi:d:e:", ["ifile=", "dfile=", "efile="])
     except getopt.GetoptError:
-        print('correctOutput.py -i <inputTEXTfile> -d <inputJSONfile> -')
+        print('correctOutput.py -i <inputTEXTfile> -d <inputFolder>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('correctOutput.py -i <inputTEXTfile> -d <inputJSONfile>')
+            print('correctOutput.py -i <inputTEXTfile> -d <inputFolder>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             input_file = arg
         elif opt in ("-d", "--dfile"):
-            entity_dictionary = json.load(open(arg, encoding="utf-8"))
+            if arg[-1] != "/": arg += '/'
+            entity_dictionary = json.load(open(arg + "dict.json", encoding="utf-8"))
+            entity_type_file = [abc.rstrip() + "[\d]+" for abc in open(arg + "entityTypes.txt", encoding="utf-8")]
     if len(opts) < 2:
         print("Number of arguments should be 2. Type '-h' for help")
         sys.exit(2)
+
     output_folder = "/".join(input_file.split("/")[:-1]) + "/"
     input_file_name = input_file.split("/")[-1][2:]
+    pattern_string = "|".join(entity_type_file)
 
     print(len(entity_dictionary))
-    pattern = re.compile(r'[A-Z]+[\d]+')
+    pattern = re.compile(pattern_string)
     f1 = open(input_file, encoding="utf-8")
     f2 = open(output_folder + input_file_name, 'w', encoding="utf-8")
     for l in f1:
